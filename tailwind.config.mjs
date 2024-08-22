fb760d62331ddf7df4b5b44042e3ec18ec1c1342
Nikos Tsilas <tsilasnikos@gmail.com>
2024-08-22 12:09:14 +0300

    Updating Navbar and creating the contact us page (I may change it sometime)

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,11 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			colors: {
+				'blanched-almond': 	'#FFEBCD',
+				'dropdown-bg': '#5375A9',
+				
+			  },
 			width: {
 				'380px': '380px'
 			}
