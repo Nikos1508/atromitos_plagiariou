61946102aaa72915f1ea8f6db2639c3dba947dc8
Nikos Tsilas <tsilasnikos@gmail.com>
2024-08-22 13:21:09 +0300

    Trying to get dynamic routing working

---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { z, defineCollection } from 'astro:content';
+
+const matchesCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        teams: z.string(),
+        score: z.string(),
+        match: z.string()
+    }),
+  });
+
+  export const collections = {
+    'matches': matchesCollection,
+  };
