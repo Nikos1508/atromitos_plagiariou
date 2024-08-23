5a1b5d6b6c76ef8d0da28b39201e94e8655ab5a3
Nikos Tsilas <tsilasnikos@gmail.com>
2024-08-23 22:10:58 +0300

    More changes

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,6 +8,76 @@ const matchesCollection = defineCollection({
     }),
   });
 
+const championshipCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+      teams: z.object({
+        1: z.string(),
+        2: z.string(),
+        3: z.string(),
+        4: z.string(),
+        5: z.string(),
+        6: z.string(),
+        7: z.string(),
+        8: z.string(),
+        9: z.string(),
+        10: z.string(),
+        11: z.string(),
+        12: z.string(),
+        13: z.string(),
+        14: z.string(),
+        15: z.string(),
+        16: z.string(),
+        17: z.string(),
+        18: z.string(),
+        19: z.string()
+      }),
+      points: z.object({
+        1: z.string(),
+        2: z.string(),
+        3: z.string(),
+        4: z.string(),
+        5: z.string(),
+        6: z.string(),
+        7: z.string(),
+        8: z.string(),
+        9: z.string(),
+        10: z.string(),
+        11: z.string(),
+        12: z.string(),
+        13: z.string(),
+        14: z.string(),
+        15: z.string(),
+        16: z.string(),
+        17: z.string(),
+        18: z.string(),
+        19: z.string()
+      }),
+      goals: z.object({
+        1: z.string(),
+        2: z.string(),
+        3: z.string(),
+        4: z.string(),
+        5: z.string(),
+        6: z.string(),
+        7: z.string(),
+        8: z.string(),
+        9: z.string(),
+        10: z.string(),
+        11: z.string(),
+        12: z.string(),
+        13: z.string(),
+        14: z.string(),
+        15: z.string(),
+        16: z.string(),
+        17: z.string(),
+        18: z.string(),
+        19: z.string()
+      }),
+    }),
+  });
+
   export const collections = {
     'match': matchesCollection,
+    'championship': championshipCollection
   };
