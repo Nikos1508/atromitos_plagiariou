1c913600e4fae7955f7e23bff3f3e4675329a605
Nikos Tsilas <tsilasnikos@gmail.com>
2024-08-25 11:51:25 +0300

    I FIXED THE PROBLEM, yay. And small changes

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,7 @@
 import { z, defineCollection } from 'astro:content';
 
 const matchesCollection = defineCollection({
-    type: 'content',
+    type: 'data',
     schema: z.object({
         teams: z.string(),
         score: z.string(),
@@ -10,71 +10,11 @@ const matchesCollection = defineCollection({
   });
 
 const championshipCollection = defineCollection({
-    type: 'content',
+    type: 'data',
     schema: z.object({
-      teams: z.object({
-        1: z.string(),
-        2: z.string(),
-        3: z.string(),
-        4: z.string(),
-        5: z.string(),
-        6: z.string(),
-        7: z.string(),
-        8: z.string(),
-        9: z.string(),
-        10: z.string(),
-        11: z.string(),
-        12: z.string(),
-        13: z.string(),
-        14: z.string(),
-        15: z.string(),
-        16: z.string(),
-        17: z.string(),
-        18: z.string(),
-        19: z.string()
-      }),
-      points: z.object({
-        1: z.string(),
-        2: z.string(),
-        3: z.string(),
-        4: z.string(),
-        5: z.string(),
-        6: z.string(),
-        7: z.string(),
-        8: z.string(),
-        9: z.string(),
-        10: z.string(),
-        11: z.string(),
-        12: z.string(),
-        13: z.string(),
-        14: z.string(),
-        15: z.string(),
-        16: z.string(),
-        17: z.string(),
-        18: z.string(),
-        19: z.string()
-      }),
-      goals: z.object({
-        1: z.string(),
-        2: z.string(),
-        3: z.string(),
-        4: z.string(),
-        5: z.string(),
-        6: z.string(),
-        7: z.string(),
-        8: z.string(),
-        9: z.string(),
-        10: z.string(),
-        11: z.string(),
-        12: z.string(),
-        13: z.string(),
-        14: z.string(),
-        15: z.string(),
-        16: z.string(),
-        17: z.string(),
-        18: z.string(),
-        19: z.string()
-      }),
+      teams: z.array( z.string() ),
+      points: z.array( z.string() ),
+      goals: z.array( z.string() )
     }),
   });
 
